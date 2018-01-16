e8972ae50b2cabb19c5a33c7429aaf00dd629ca9
Shishir <shishirmax@gmail.com>
2018-01-16 21:22:19 +0530

    script updated

--- dbconnect.py
+++ dbconnect.py
@@ -1,5 +1,10 @@
 import pypyodbc
 connection = pypyodbc.connect('Driver={SQL Server};Server=SHISHIRS;Database=shishir;uid=sa;pwd=sysadmin')
-print("Connected")
-
+#print("Connected")
+cursor = connection.cursor()
+SQLCommand = ("select * from product")
+res = cursor.execute(SQLCommand)
+for r in res:
+	print(r)
+connection.commit()
 connection.close()
