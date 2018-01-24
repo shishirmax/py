cf102f2f5e33a5e998534a1cef396ca39414cdc2
Shishir <shishirmax@gmail.com>
2018-01-24 19:49:03 +0530

    new python script added'

---
+++ datetime.py
@@ -0,0 +1,3 @@
+from datetime import datetime
+dt = str(datetime.datetime.now())
+print(dt)
---
+++ nametest.py
@@ -0,0 +1,2 @@
+record_distance = string_distance('bob roberts 1600 pennsylvania ave. 555-0123','Robert Roberts 1600 Pensylvannia Avenue')
+print (record_distance)
