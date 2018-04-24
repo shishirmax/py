df08610f8127e2a682e8db03385e77aaeb5e73d7
Shishir <shishirmax@gmail.com>
2018-04-24 21:50:48 +0530

    initial commit

---
+++ practice/commandLine_Arg.py
@@ -0,0 +1,3 @@
+import sys
+
+print("Name of Script:",sys.argv[0])
---
+++ practice/function_arg.py
@@ -0,0 +1,5 @@
+def sayHello(name,msg):
+    """jhgljkdgh
+    dgjkhdfgjklh"""
+    print("Hello",name+', '+msg)
+sayHello("Shishir","Let's take some snap!!!")
