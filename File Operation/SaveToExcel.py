34a4fc37d376529a71e2444d20375136fe55a58d
Shishir <shishirmax@gmail.com>
2019-03-06 10:13:05 +0530

    Export Data to Excel File

--- File Operation/SaveToExcel.py
+++ File Operation/SaveToExcel.py
@@ -3,3 +3,5 @@ import pandas as pd
 connection = pyodbc.connect('Driver={SQL Server};Server=SHISHIRS;Database=shishir;uid=sa;pwd=sysadmin')
 data = pd.read_sql('SELECT * FROM Employee',connection)
 data.to_excel('EmployeeData.xls')
+
+#note: on execution this script if it gives error like "ModuleNotFoundError: No module named 'xlwt'" then run pip install xlwt
