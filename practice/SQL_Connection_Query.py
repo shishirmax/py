e4e911284219c11044aace155a3deb507a80818f
Shishir <shishirmax@gmail.com>
2019-03-07 17:52:47 +0530

    script updated

--- practice/SQL_Connection_Query.py
+++ practice/SQL_Connection_Query.py
@@ -1,6 +1,8 @@
 import pyodbc
 import pandas as pd
 connection = pyodbc.connect('Driver={SQL Server};Server=SHISHIRS;Database=AdventureWorks;uid=sa;pwd=sysadmin')
+#data = pd.read_sql('SELECT COUNT(1) TotalRecords, CAST(SessionnStart As DATE) As Dates from homeSpotter.DimSession group by CAST(SessionnStart AS DATE) order by CAST(SessionnStart AS DATE) DESC')
+#data.to_excel("DailyDataCount.xlsx",sheet_name='Records_Day')
 data = pd.read_sql('SELECT AddressID,AddressLine1,AddressLine2,City FROM Person.Address',connection)
 #data.to_excel('address.xlsx',encoding='sys.getfilesystemencoding()')
 data.to_excel("address.xlsx",sheet_name='Sheet_name_1')
